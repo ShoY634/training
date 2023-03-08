b31c5ac0231bb929352a106c09128ecaa7361ca7
ShoY634 <shota20000228@gmail.com>
2023-03-08 17:49:23 +0900

    list,dicなどの練習

--- python/2023_03_08.py
+++ python/2023_03_08.py
@@ -1,7 +1,7 @@
 # for i in range(10):
 #     print(i)
     
-list = ["Apple", "Orange", "Banana", "Grape"]
+# list = ["Apple", "Orange", "Banana", "Grape"]
 
 # for name in list:
 #     if name == "Banana":
@@ -72,17 +72,55 @@ list_cost = [100, 150, 50, 200]
     
 # -----------------------------------------------
 
-squares = [i**2 for i in range(5)]
-print(squares)
+# squares = [i**2 for i in range(5)]
+# print(squares)
 
-odds = [i for i in range(10) if i % 2 == 1]
-print(odds)
+# odds = [i for i in range(10) if i % 2 == 1]
+# print(odds)
 
-multiple3 = [i*3 for i in range(10)]
-print(multiple3)
+# multiple3 = [i*3 for i in range(10)]
+# print(multiple3)
 
-odd_even = ["odd" if i % 2 == 1 else "even" for i in range(10)]
-print(odd_even)
+# odd_even = ["odd" if i % 2 == 1 else "even" for i in range(10)]
+# print(odd_even)
 
-nabeatsu = [i for i in range(50) if not i % 3 == 0]
-print(nabeatsu)
+# nabeatsu = [i for i in range(50) if not i % 3 == 0]
+# print(nabeatsu)
+
+# --------------------------------
+
+list_ramen = ["syoramen", "tyuramen", "dairamen", "tokudairamen"]
+list_ramen_cost = [100, 150, 300]
+
+# menu = {}
+# for ramen, cost in zip(list_ramen, list_ramen_cost):
+#     menu[ramen] = cost
+    
+# print(menu)
+
+def ramen_menu(namelist, costlist):
+    menu = {}
+    while True:
+        if len(namelist) > len(costlist):
+            costlist.append(1000)
+            costlist = costlist + [1000]
+            
+        else:
+            len(namelist) == len(costlist)
+            break
+    
+    for name, cost in zip(namelist, costlist):
+        menu[name] = cost
+        
+    return menu
+
+# print(ramen_menu(list_ramen, list_ramen_cost))
+
+# -------------------------------------------------
+
+menu = {'syoramen': 100, 'tyuramen': 150, 'dairamen': 300, 'tokudairamen': 1000}
+
+print(menu.values())
+a = menu.values()
+menu_values_list = list(menu.values())
+print(menu_values_list)
