05b3d723d6b0f0c28ea0736e5e3d3ea5e1a9db5c
ShoY634 <shota20000228@gmail.com>
2023-03-09 00:01:13 +0900

    クラス練習を修正

--- python/2023_03_08-2.py
+++ python/2023_03_08-2.py
@@ -1,85 +1,37 @@
-class Animal(object):
-    def __init__(self, sex_animals, age_animals):
-        self._sex_animals = sex_animals
-        self._age_animals = age_animals
-        
-    @property
-    def sex_animals(self):
-        return self._sex_animals
-    
-    @sex_animals.setter
-    def sex_animals(self, Sex):
-        self.sex = Sex
-    
-    @property
-    def age_animals(self):
-        return self._age_animals
-    
-    @age_animals.setter
-    def age_animals(self, Age):
-        self.age_animals = Age
-    
-        
-class Human(Animal):
-    def __init__(self, sex, age, name, nation, job):
+class Animal:
+    def __init__(self, sex, age):
         self.sex = sex
         self.age = age
-        self._name = name
-        self._nation = nation
-        self._job = job
-        
-    @property
-    def name(self):
-        return self._name
+
+
+class Human(Animal):
+    def __init__(self, sex, age, name, nation, job):
+        super().__init__(sex, age)
+        self.name = name
+        self.nation = nation
+        self.job = job
     
-    @name.setter
-    def name(self, Name):
-        self.name = Name
-    
-    @property
-    def nation(self):
-        return self._nation
-    
-    @nation.setter
-    def nation(self, Nation):
-        self.nation = Nation
-    
-    @property
-    def job(self):
-        return self._job
-        
-    @job.setter
-    def job(self, Job):
-        self.job = Job
-        
     def info(self):
         print(f"sex:{self.sex} age:{self.age} name:{self.name} nation:{self.nation} job:{self.job}")
-    
-    
+
+
 class Dogs(Animal):
-    def __init__(self, sex_dogs, age_dogs, species_dogs):
-        self.sex_dogs = sex_dogs
-        self.age_dogs = age_dogs
-        self._species_dogs = species_dogs
-        
-    @property
-    def species_dogs(self):
-        return self._species_dogs
-    
-    @species_dogs.setter
-    def species_dogs(self, Species):
-        self.species_dogs = Species
+    def __init__(self, sex, age, species):
+        super().__init__(sex, age)
+        self.species = species
         
     def info(self):
-        print(f"sex:{self.sex_dogs} age:{self.age_dogs} species:{self.species_dogs}")
-    
-    
+        print(f"sex:{self.sex} age:{self.age} species:{self.species}")
+
+
 shota = Human('male', 23, 'Shota', 'japan', 'engineer')
 shota.info()
+
 ginjiro = Human('male', 24, 'Ginjiro', 'japan', 'engineer')
 ginjiro.info()
 
 chihuahua = Dogs('male', 2, 'chihuahua')
 chihuahua.info()
+
 dachshund = Dogs('female', 1, 'dachshund')
 dachshund.info()
