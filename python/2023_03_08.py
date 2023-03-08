94b7531c944bc01cd0053290ead621f104c51745
ShoY634 <shota20000228@gmail.com>
2023-03-08 14:26:05 +0900

    for練習

---
+++ python/2023_03_08.py
@@ -0,0 +1,39 @@
+# for i in range(10):
+#     print(i)
+    
+list = ["Apple", "Orange", "Banana", "Grape"]
+
+# for name in list:
+#     if name == "Banana":
+#         break
+#     print (name)
+
+
+# for name in list:
+#     if name == "Banana":
+#         print("monkey")
+#         continue
+#     print(name)
+    
+    
+# for name in list:
+#     print(name)
+
+# else:
+#     print("end")
+    
+    
+# count = 0
+# for name in list:
+#     if count == 3:
+#         break
+    
+#     print(name)
+#     count += 1
+
+# else:
+#     print("I'm full stomach")
+    
+    
+for i in range(5, 20, 3):
+    print(i)
