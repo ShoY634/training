63e19853533ee7b1b2510b88ea4d4243a0f15f00
ShoY634 <shota20000228@gmail.com>
2023-03-10 10:22:21 +0900

    for文思い出しの雑記

---
+++ python/2023_03_10.py
@@ -0,0 +1,11 @@
+# ３の倍数で鳴き声が変わるきんにくん
+
+count = 1
+for i in range(10):
+    if count % 3 == 0:
+        print("power-!")
+    else:
+        print('ya-!')
+    count += 1
+    
+# ------------------------------
